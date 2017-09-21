ca61971132f3d2c7a1c410106532066c47832421
William Bergmann Børresen <william@borresen.one>
2017-09-21 07:52:07 +0200

    new exc added

---
+++ Tree/Tree/Tree/Tree.cpp
@@ -0,0 +1,20 @@
+// Tree.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "AvlTree.h"
+#include "BinarySearchTree.h"
+
+
+int main()
+{
+	BinarySearchTree Tree;
+
+	for (int i = 0; i < 100; i++)
+	{
+		Tree.insert(rand() % 10 + 1);
+	}
+	Tree.printTree();
+    return 0;
+}
+
